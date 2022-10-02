cf8b4e4a3f561b39881314818c4f5f79e7417b9d
Bianca11810 <Bianca11810@gmail.com>
2022-10-02 21:18:02 +0000

    My first commit

---
+++ src/js/component/TrafficLight.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+const Home = () => {
+	const [selectedColor, setSelectedColor] = React.useState('yellow')
+	return (
+		<div className="container">
+			<div className="trafficLight">
+				<div 
+					onClick={() => setSelectedColor('red')}
+					className={'light red' + (selectedColor === 'red' ? ' glowRed' : '')}>
+				</div>
+				<div 
+					onClick={() => setSelectedColor('yellow')}
+					className={'light yellow' + (selectedColor === 'yellow' ? ' glowYellow' : '')}>
+
+				</div>
+				<div 
+					onClick={() => setSelectedColor('green')}
+					className={'light green' + (selectedColor === 'green' ? ' glowGreen' : '')}>
+					
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Home;
